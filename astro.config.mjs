ca610b70c8c2ce18ff4eb4701a524fbc65ece7e5
p1atypu5 <neurocarrot@gmail.com>
2026-03-09 16:50:33 +0300

    added deploy files for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://p1atypu5.github.io",
+  base: "/final-statement",
+});
